dd691b90fbaaa41b94a423c9080dbdd73386661a
gbelda <gbelda56@gmail.com>
2021-10-27 16:17:34 +0200

    Added functions docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,7 @@ let resultEl = document.querySelector('.result')
 let countdown = setInterval(function () {
     document.querySelector('.timer').innerHTML = timer
     timer--
+
     if (timer == -2) {
         clearInterval(countdown)
         document.querySelector('.timer').innerHTML = ''
@@ -31,23 +32,34 @@ let countdown = setInterval(function () {
         for (let i = 0; i < generatedNumbers.length; i++) {
             let userNumber = parseInt(prompt('inserisci uno dei numeri della sequenza precedente'))
             if (generatedNumbers.includes(userNumber)) {
-                if (!result.includes(userNumber)) {
-                    result.push(userNumber)
-                }
+                compareAndPushInArray(result, userNumber)
             }
         }
         /* Dopo che sono stati inseriti i 5 numeri,
         il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
-        resultEl.innerHTML = `Punteggio = ${result.length}.
-         Numeri ricordati: ${result}`
+        resultEl.innerHTML = `Punteggio = ${result.length}. <br>
+         I numeri ricordati: ${result} <br>`
     }
 
 }, 1000);
 
+/**
+ * Checks if a number is already included in an array and if not, it gets included.
+ * @param {array} array - The array where we put the element
+ * @param {number} number - Number to be compared with element inside array
+ */
+function compareAndPushInArray(array, number) {
+    if (!array.includes(number)) {
+        array.push(number)
+    }
+}
 
-
-
-
+/**
+ * Generates a random number between a min and max
+ * @param {number} min - min value of result
+ * @param {numuber} max - maximum value of result
+ * @returns - random number
+ */
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
